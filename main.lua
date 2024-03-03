3d7f4a90a22559f14a51af1d6f72070f6430e33d
altair823 <dlsrks0734@gmail.com>
2024-03-04 06:34:10 +0900

    Fix block toggling logic in love.mousemoved() function

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ function love.load()
     love.window.setMode(1024, 900, {resizable=false, vsync=false, minwidth=400, minheight=300})
     love.window.setTitle("Mine Sweeper")
 
-    
+
     board = Board:new(0, 0, 1024, 700, boardAtlas)
     field = Field:new(20, 20, 80)
     board:setBlockMatrix(field, cellAtlas, blockAtlas, 32, 32)
@@ -19,6 +19,8 @@ function love.draw()
     board:draw()
 end
 
+prevLoc = {i = 0, j = 0}
+
 function love.mousemoved(x, y, dx, dy, istouch)
     mouseLocation.x = x
     mouseLocation.y = y
@@ -26,21 +28,19 @@ function love.mousemoved(x, y, dx, dy, istouch)
     and y >= board.center.y - (board.field.yCount * 32) / 2 and y <= board.center.y + (board.field.yCount * 32) / 2 - 1 then
         local i = math.floor((x - (board.center.x - (board.field.xCount * 32) / 2)) / 32) + 1
         local j = math.floor((y - (board.center.y - (board.field.yCount * 32) / 2)) / 32) + 1
-        board.blockMatrix[i][j]:toggle()
-        
-        -- untoggle other blocks
-        for ii=1, board.field.xCount do
-            for jj=1, board.field.yCount do
-                if ii ~= i or jj ~= j then
-                    board.blockMatrix[ii][jj]:untoggle()
-                end
+        if prevLoc.i ~= i or prevLoc.j ~= j then
+            if prevLoc.i ~= 0 and prevLoc.j ~= 0 then
+                board.blockMatrix[prevLoc.i][prevLoc.j]:untoggle()
             end
+            board.blockMatrix[i][j]:toggle()
+            prevLoc.i = i
+            prevLoc.j = j
         end
     else
-        for i=1, board.field.xCount do
-            for j=1, board.field.yCount do
-                board.blockMatrix[i][j]:untoggle()
-            end
+        if prevLoc.i ~= 0 and prevLoc.j ~= 0 then
+            board.blockMatrix[prevLoc.i][prevLoc.j]:untoggle()
+            prevLoc.i = 0
+            prevLoc.j = 0
         end
     end
 end
