4dbcabdc3763d9d138c5eef0c725b8e42d2de45b
altair823 <kimth0734@gmail.com>
2024-03-09 20:38:23 +0900

    Update window mode and resource sizes

--- common/conf.lua
+++ common/conf.lua
@@ -2,6 +2,6 @@
 -- PROFILER = true
 
 DefaultWindowSize = {width = 1600, height = 900, minwidth = 400, minheight = 300}
-love.window.setMode(DefaultWindowSize.width, DefaultWindowSize.height, {resizable=true, vsync=false, minwidth=400, minheight=300})
+love.window.setMode(DefaultWindowSize.width, DefaultWindowSize.height, {fullscreen = false, resizable=true, vsync=false, minwidth=400, minheight=300})
 
 SpriteRatio = 1
