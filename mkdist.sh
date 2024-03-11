2dfcc7712b15e7426804dd0eec57bfd4a75219d5
altair823 <kimth0734@gmail.com>
2024-03-11 22:37:17 +0900

    Fix bugs and update distribution script

--- mkdist.sh
+++ mkdist.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+echo "Creating .love file..."
+zip -r minesweeper_love.love \
+*.lua \
+object \
+model \
+game \
+common \
+resources \
+original_resources \
+fonts
+
 echo "Creating .exe file..."
 cat love.exe minesweeper_love.love > ./dist/minesweeper_love.exe
+rm minesewwper_love.love
 
 echo "Compresing .exe directory..."
 zip -r ./minesweeper_love.zip ./dist
